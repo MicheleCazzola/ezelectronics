8b8fc806037666a8b71e5e220a05cc60b8cea5bc
Giuseppe Vacante s332155 <s332155@studenti.polito.it>
2024-06-08 16:44:51 +0200

    Unit test: product DAO testing.

--- code/server/test_unit/dao_tests/product.test.ts
+++ code/server/test_unit/dao_tests/product.test.ts
@@ -1,13 +1,15 @@
 import { describe, test, expect, beforeAll, afterAll, jest, beforeEach, afterEach } from "@jest/globals"
 import ProductDAO from "../../src/dao/productDAO";
 import db from "../../src/db/db";
-import { Category } from "../../src/components/product";
+import { Category, Product } from "../../src/components/product";
 import CartDAO from "../../src/dao/cartDAO";
 import { ProductAlreadyExistsError, ProductNotFoundError } from "../../src/errors/productError";
 import { Database } from "sqlite3";
 import { hasUncaughtExceptionCaptureCallback } from "node:process";
 import { mock } from "node:test";
 import dayjs from "dayjs";
+import { callbackify } from "node:util";
+import { error } from "node:console";
 
 
 jest.mock("../../src/db/db.ts");
@@ -196,21 +198,21 @@ describe("ProductDao test:", () => {
 
         });
 
-        test('should return null if the product does not exist', async () => {
-            const testModel = 'TestModel';
-
-            const mockDBGet = jest.spyOn(db, 'get').mockImplementation((sql, params, callback) => {
-                callback(null, null);
+        test("it should throw an ProductNotFoundError if model does not represent a product in the database", async () => {
+            
+            const testModel = "NotExistingModel";
+            const errMessage = new Error("UNIQUE constraint failed: product.model");
+            
+            const mockDBGet = jest.spyOn(db, "get").mockImplementation((sql, params, callback) => {
+                callback(errMessage, null);
                 return {} as Database;
             });
-    
+
             const dao = new ProductDAO();
-            const result = await dao.getProductByModel(testModel);
-    
+
+            await expect(dao.getProductByModel(testModel)).rejects.toThrow(ProductNotFoundError);
             expect(mockDBGet).toBeCalledTimes(1);
-            expect(mockDBGet).toBeCalledWith(expect.any(String), [testModel], expect.any(Function));
-            expect(result).toBeNull();
-        
+
         });
 
 
@@ -222,9 +224,10 @@ describe("ProductDao test:", () => {
             
             const testModel = "NotExistingModel";
             const soldQuantity = 5;
-            
+            const errMessage = new Error("UNIQUE constraint failed: product.model");
+
             const mockDBGet = jest.spyOn(db, "get").mockImplementation((sql, params, callback) => {
-                callback(null, null);
+                callback(errMessage, null);
                 return {} as Database;
             });
 
@@ -393,9 +396,11 @@ describe("ProductDao test:", () => {
             
             const testModel = "NotExistingModel";
             const soldQuantity = 5;
+            const errMessage = new Error("UNIQUE constraint failed: product.model");
+
             
             const mockDBGet = jest.spyOn(db, "get").mockImplementation((sql, params, callback) => {
-                callback(null, null);
+                callback(errMessage, null);
                 return {} as Database;
             });
 
@@ -554,33 +559,387 @@ describe("ProductDao test:", () => {
           });
 
     });
-    /*
-
-    TO BE COMMITTED 
-    
+   
     describe("getAllProducts test:", () => {
+
+        test("it should throw an ProductNotFoundError if model does not represent a product in the database", async () => {
+            
+            const testModel = "NotExistingModel";
+            const errMessage = new Error("UNIQUE constraint failed: product.model");            
+            
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+                callback(errMessage, null);
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            await expect(dao.getAllProducts("Model", testModel, null)).rejects.toThrow(ProductNotFoundError);
+            expect(mockDBAll).toBeCalledTimes(1);
+
+        });
+
+        test("it should return all products without filers", async () =>{
+
+            const testResultProducts = [
+                { SellingPrice: 123, Model: "TestModel1", Category: Category.SMARTPHONE, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+                { SellingPrice: 123, Model: "TestModel3", Category: Category.APPLIANCE, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 }
+            ];
+            
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) =>{
+                callback(null, testResultProducts);
+                return {} as Database;
+            });
+
+            const testProducts = testResultProducts.map(p => 
+                new Product(p.SellingPrice, p.Model, p.Category, p.ArrivalDate, p.Details, p.AvailableQuantity)
+            );
+
+            const dao = new ProductDAO();
+            //without filters:
+            const result = await dao.getAllProducts(null, null, null);
+
+            expect(result).toEqual(testProducts);
+            expect(mockDBAll).toBeCalledTimes(1);
+        });
+
+        test("it should return products filtered by category", async () => {
+
+            /*
+            const testProducts1 = [
+                { SellingPrice: 123, Model: "TestModel1", Category: Category.SMARTPHONE, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+                { SellingPrice: 123, Model: "TestModel3", Category: Category.APPLIANCE, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 }
+            ]
+            
+            const testProducts2 = [
+                new Product(123, "TestModel1", Category.SMARTPHONE, "2024-06-06", "testDetails", 10),
+                new Product(123, "TestModel2", Category.LAPTOP, "2024-06-06", "testDetails", 10),
+                new Product(123, "TestModel3", Category.APPLIANCE, "2024-06-06", "testDetails", 10)
+            ];
+            */
+
+            const categoryFiltered = [
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+            ]
+
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+              
+                callback(null, categoryFiltered);
+                return {} as Database;
+            })
+
+            const testProducts = categoryFiltered.map(p => 
+                new Product(p.SellingPrice, p.Model, p.Category, p.ArrivalDate, p.Details, p.AvailableQuantity)
+            );
+
+
+            const dao = new ProductDAO();
+            //Category filters:
+            const result = await dao.getAllProducts("Category", "Laptop", null);
+
+            expect(result).toEqual(testProducts);
+            expect(mockDBAll).toBeCalledTimes(1);
+            
+
+        });
+
+        test("it should return products filtered by model", async () => {
+            const modelFiltered = [
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+            ]
+
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+                callback(null, modelFiltered);
+                return {} as Database;
+            })
+
+            const testProducts = modelFiltered.map(p => 
+                new Product(p.SellingPrice, p.Model, p.Category, p.ArrivalDate, p.Details, p.AvailableQuantity)
+            );
+
+
+            const dao = new ProductDAO();
+            //Model filters:
+            const result = await dao.getAllProducts("Model", null, "TestModel2");
+
+            expect(result).toEqual(testProducts);
+            expect(mockDBAll).toBeCalledTimes(1);
+            
+        });
+    
+        test("it should handle database error", async () => {
+            const error = new Error("Database error");
+
+            const mockDBAll = jest.spyOn(db, 'all').mockImplementation((sql, params, callback) => {
+              callback(error, null);
+              return {} as Database;
+            });
+        
+            const dao = new ProductDAO();
+        
+            await expect(dao.getAllProducts(null, null, null)).rejects.toThrow(error);
+            expect(mockDBAll).toBeCalledTimes(1);
+    
+
+        });
 
 
     });
 
     describe("getAllAvailableProducts test:", () => {
 
+        test("it should throw an ProductNotFoundError if model does not represent a product in the database", async () => {
+            
+            const testModel = "NotExistingModel";
+            const errMessage = new Error("UNIQUE constraint failed: product.model");
+
+            
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+                callback(errMessage, null);
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            await expect(dao.getAllAvailableProducts("Model", testModel, null)).rejects.toThrow(ProductNotFoundError);
+            expect(mockDBAll).toBeCalledTimes(1);
+
+        });
+    
+        test("it should return all available products without filters", async () => {
+
+            const testProducts1 = [
+                { SellingPrice: 123, Model: "TestModel1", Category: Category.SMARTPHONE, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+                { SellingPrice: 123, Model: "TestModel3", Category: Category.APPLIANCE, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 }
+            ]
+
+            const expectedProducts = testProducts1.filter(p => p.AvailableQuantity > 0);
+
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+                callback(null, expectedProducts);
+                return {} as Database;
+            })
+
+            const expectResult = expectedProducts.map(p => new Product(p.SellingPrice, p.Model, p.Category, p.ArrivalDate, p.Details, p.AvailableQuantity));
+
+            const dao = new ProductDAO();
+            const result = await dao.getAllAvailableProducts(null, null, null);
+
+            expect(result).toEqual(expectResult);
+            expect(mockDBAll).toBeCalledTimes(1);
+
+        });
+
+        test("it should return available products filtered by category", async () => {
+        
+            const categoryFiltered = [
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+            ]
+
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+        
+                callback(null, categoryFiltered);
+                return {} as Database;
+            })
+
+            const testProducts = categoryFiltered.map(p => 
+                new Product(p.SellingPrice, p.Model, p.Category, p.ArrivalDate, p.Details, p.AvailableQuantity)
+            );
+
+
+            const dao = new ProductDAO();
+            //Category filters:
+            const result = await dao.getAllAvailableProducts("Category", "Laptop", null);
+
+            expect(result).toEqual(testProducts);
+            expect(mockDBAll).toBeCalledTimes(1);
+            
+
+        });
+
+        
+        test("it should return available products filtered by model", async () => {
+
+            const modelFiltered = [
+                { SellingPrice: 123, Model: "TestModel2", Category: Category.LAPTOP, ArrivalDate: "2024-06-06", Details: "testDetails", AvailableQuantity: 10 },
+            ]
+
+            const mockDBAll = jest.spyOn(db, "all").mockImplementation((sql, params, callback) => {
+                callback(null, modelFiltered);
+                return {} as Database;
+            })
+
+            const testProducts = modelFiltered.map(p => 
+                new Product(p.SellingPrice, p.Model, p.Category, p.ArrivalDate, p.Details, p.AvailableQuantity)
+            );
+
+
+            const dao = new ProductDAO();
+            //Model filters:
+            const result = await dao.getAllAvailableProducts("Model", null, "TestModel2");
+
+            expect(result).toEqual(testProducts);
+            expect(mockDBAll).toBeCalledTimes(1);
+
+        });
+        
+        test("it should handle database errors", async () => {
+
+            const error = new Error("Database error");
+
+            const mockDBAll = jest.spyOn(db, 'all').mockImplementation((sql, params, callback) => {
+              callback(error, null);
+              return {} as Database;
+            });
+        
+            const dao = new ProductDAO();
+        
+            await expect(dao.getAllProducts(null, null, null)).rejects.toThrow(error);
+            expect(mockDBAll).toBeCalledTimes(1);
+    
+        });
+
     });
 
     describe("deleteProducts test:", () => {
+
+        test("it should delete all products and resolve to true", async () => {
+
+            const mockDBRun = jest.spyOn(db, 'run').mockImplementation((sql, params, callback) => {
+                callback(null);  
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+            const result = await dao.deleteProducts();
+
+            expect(result).toBe(true);
+            expect(mockDBRun).toBeCalledTimes(1);
+        });
+
+        test("It should reject if there is an error during delection", async () => {
+           
+            const error = new Error("Database error");
+
+            const mockDBRun = jest.spyOn(db, 'run').mockImplementation((sql, params, callback) => {
+                callback(error);  
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            await expect(dao.deleteProducts()).rejects.toThrow(error); 
+            expect(mockDBRun).toBeCalledTimes(1);
+
+        });
+
+
 
     });
 
     describe("deleteProductByModel test:", () => {
 
+        test("it should throw an ProductNotFoundError if model does not represent a product in the database", async () => {
+            
+            const testModel = "NotExistingModel";
+            
+            const errMessage = new Error("UNIQUE constraint failed: product.model");
+
+            const mockDBRun = jest.spyOn(db, "run").mockImplementation((sql, params, callback) => {
+                callback(errMessage);
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            await expect(dao.deleteProductByModel(testModel)).rejects.toThrow(ProductNotFoundError);
+            expect(mockDBRun).toBeCalledTimes(1);
+
+        });
+
+
+        test("It should return true if the product has been deleted", async () =>{
+
+            const testModel = "TestModel";
+
+            const mockDBRun = jest.spyOn(db, "run").mockImplementationOnce((sql, params, callback) => {
+                callback(null);
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            const result = await dao.deleteProductByModel(testModel);
+
+            expect(result).toBe(true);
+            expect(mockDBRun).toBeCalledTimes(1);
+
+
+        });
+
+
+        test("It should reject if there is an error during delection", async () => {
+           
+            const error = new Error("Database error");
+
+            const mockDBRun = jest.spyOn(db, 'run').mockImplementation((sql, params, callback) => {
+                callback(error);  
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            await expect(dao.deleteProducts()).rejects.toThrow(error); 
+            expect(mockDBRun).toBeCalledTimes(1);
+
+        });
+
+
+
     });
 
     describe("getProductQuantity test:", () => {
 
+        test("it should throw an ProductNotFoundError if model does not represent a product in the database", async () => {
+            
+            const testModel = "NotExistingModel";
+            
+            const errMessage = new Error("UNIQUE constraint failed: product.model");
+
+            const mockDBGet = jest.spyOn(db, "get").mockImplementation((sql, params, callback) => {
+                callback(errMessage, null);
+                return {} as Database;
+            });
+
+            const dao = new ProductDAO();
+
+            await expect(dao.getProductQuantity(testModel)).rejects.toThrow(ProductNotFoundError);
+            expect(mockDBGet).toBeCalledTimes(1);
+
+        });
+
+        test("it should return the available quantity of the product", async () =>{
+
+            const testModel = "TestModel";
+            const testQuantity = 10;
+
+            const mockDBGet = jest.spyOn(db, "get").mockImplementation((sql, params, callback) => {
+                callback(null, {AvailableQuantity: testQuantity });
+                return {} as Database;
+            });
+        
+            const dao = new ProductDAO();
+            const result = await dao.getProductQuantity(testModel);
+            
+            expect(result).toBe(testQuantity);
+            expect(mockDBGet).toBeCalledTimes(1);
+
+        });
+
     });
-
-*/
-
 
 })
 
