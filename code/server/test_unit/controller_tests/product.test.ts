556244d0612dae923a75375c33c3f0c3c8582171
Giuseppe Vacante s332155 <s332155@studenti.polito.it>
2024-06-11 16:29:07 +0200

    readded productByModel controller test

--- code/server/test_unit/controller_tests/product.test.ts
+++ code/server/test_unit/controller_tests/product.test.ts
@@ -82,7 +82,7 @@ describe("Controller tests", () => {
             expect(response).toBe(false);
         });
     });
-
+*/
     
     describe("productByModel test: ", () => {
 
@@ -105,7 +105,7 @@ describe("Controller tests", () => {
 
 
     });
-*/
+
 
     describe("changeProductquantity test:", () => {
 
