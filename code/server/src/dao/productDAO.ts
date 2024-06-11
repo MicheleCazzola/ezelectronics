0571d9da55d002900ca85ef14ed8b63e59605501
Flaviana Schioppa <s331418@studenti.polito.it>
2024-06-11 19:57:38 +0200

    fix: modelByProduct

--- code/server/src/dao/productDAO.ts
+++ code/server/src/dao/productDAO.ts
@@ -334,13 +334,14 @@ class ProductDAO {
   async deleteProductByModel(model: string): Promise<Boolean> {
     return new Promise<boolean>((resolve, reject) => {
       try {
-        let prod = this.existsProduct(model)
-        if (!prod) {
-          reject(new ProductNotFoundError())
-          return
-        }
+        this.existsProduct(model).then(prod => {
+          if (prod == false) {
+            reject(new ProductNotFoundError)
+            return
+          }
+        })
         const sql = "DELETE FROM product_descriptor WHERE Model = ?";
-        db.run(sql, [model], (err: Error | null, row: any) => {
+        db.run(sql, [model], (err: Error | null) => {
           try {
             if (err) reject(err);
             resolve(true);
